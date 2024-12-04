6c23bf5fcc7b825a35f45fc1a6cdef657063c01d
monsan18 <monicasantika@gmail.com>
2024-12-04 18:32:02 +0700

    add readme, sql

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# RUNNING STEP GOLANG SERVER ECHO FRAMEWORK 
+# RUNNING STEP GOLANG SERVER ECHO FRAMEWORK
+
+1. Open Terminal in project folder
+2. Run "go run server.go"
+3. Allow access when a promt show.
+4. Open Postman
+5. Open API documentation
+6. Run HTTP Request from Postman according to documentation.
+7. Start Apache Xampp and MySql if has not started yet.
+8. Export table to database from the .sql file.
+
+#
---
+++ test_golang_monica.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2024 at 10:19 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test_golang_monica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(8) NOT NULL,
+  `brand_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`) VALUES
+(1, ''),
+(2, ''),
+(3, 'Brand 1'),
+(4, 'Brand 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(8) NOT NULL,
+  `product_name` varchar(200) NOT NULL,
+  `price` decimal(18,2) NOT NULL,
+  `qty` int(8) NOT NULL,
+  `brand_id` int(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `price`, `qty`, `brand_id`) VALUES
+(1, 'Tes barang', 2500.00, 3, 1),
+(3, 'Tes barang 3', 20000.00, 4, 2),
+(4, 'Tes barang 4', 2100.00, 4, 2),
+(5, 'Tes barang 5', 32500.00, 4, 2),
+(6, 'Tes barang 6', 32100.00, 4, 2),
+(7, 'Tes barang 7', 4500.00, 7, 1),
+(8, 'Tes barang 8', 55399.00, 8, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

45689481b7a26e07cd87b932ca295fef81e320a8
monsan18 <monicasantika@gmail.com>
2024-12-05 13:22:57 +0700

    update readme

--- README.md
+++ README.md
@@ -7,6 +7,34 @@
 5. Open API documentation
 6. Run HTTP Request from Postman according to documentation.
 7. Start Apache Xampp and MySql if has not started yet.
-8. Export table to database from the .sql file.
+8. Create new database 'ecommerce'.
+9. Import .sql to database 'ecommerce' from the .sql file.
 
-#
+# ###############
+Sample Request : See postman collection Test_Golang_Monica.postman_collection.json.
+
+# Essay Answer #####
+
+1. Project Review adalah sebuah proses untuk meng-evaluasi keberhasilan proyek dan memutuskan akan terus menggunakan sumber daya.
+    Project review dapat dilakukan dalam beberapa bentuk, yaitu :
+    - rapat / meeting dengan tim projek untuk menilai status projek saat ini,
+    - tinjauan di akhir projek.
+
+Project Planning adalah sebuah proses perencanaan proyek yang mencakup langkah-langkah untuk menyelesaikan proyek dalam jangka waktu tertentu. Biasanya dilakukan sebelum proyek dilaksanakan. Project planning juga biasanya disajikan dalam bentuk gantt chart untuk memudahkan memvisualisasikan alur waktu proyek.
+
+2. Load balance adalah sebuah teknik yang mendistribusikan lalu lintas jaringan atau aplikasi di beberapa server. Hal tersebut bertujuan untuk meningkatkan kinerja, keandalan, dan ketersediaan. 
+    Di Amazon EC2, Elastic Load Balancing (ELB) adalah layanan load balancer yang disediakan oleh AWS.
+
+    Security Group pada AWS berperan sebagai firewall virtual untuk instance EC2, guna mengendalikan lalu lintas yang masuk dan keluar.
+    Cara kerja security group pada AWS yaitu membantu dalam mengamankan lingkungan cloud dengan mengendalikan bagaimana lalu lintas akan diizinkan masuk ke mesin EC2. 
+    Dengan security group, kita dapat memastikan bahwa semua lalu lintas yang mengalir di tingkat instans hanya melalui port dan protokol yang telah ditetapkan.
+    Seperti whitelist (daftar putih), aturan security group tidak selalu permisif. Tidak mungkin kita membuat aturan yang menolak akses.
+    Security group bersifat stateful, jadi jika permintaan masuk lolos, maka permintaan keluar juga akan lolos.
+
+3. Cara mengatasi memory leak pada Golang :
+    - menghindari referensi melingkar antara objek untuk memungkinkan pengumpulan sampah yang tepat.
+    - kelola goroutine dan saluran dengan tepat untuk mencegah kebocoran dari goroutine yang diblokir atau tidak dihentikan.
+    - lepaskan sumber daya yang tidak diperlukan seperti file, koneksi jaringan, dan saluran saat tidak lagi diperlukan.
+    - terapkan kebijakan kadaluarsa dengan time-to-live (TTL) untuk cache dan variable global.
+
+
--- models/brands.go
+++ models/brands.go
@@ -1,79 +1,77 @@
-package	models
+package models
 
 import (
 	"Test_Go_Monica/db"
-	"net/http"
 	"errors"
+	"net/http"
 )
 
-type Brands struct  {
-	BrandId	int `json:"brand_id"`
-	BrandName	string	`json:"brand_name"`
+type Brands struct {
+	BrandId   int    `json:"brand_id"`
+	BrandName string `json:"brand_name"`
 }
 
-func AddBrand(Brand_Name string) (Response, error){ // Fungsi untuk meng-inputkan data Post Method dari tabel product
+func AddBrand(Brand_Name string) (Response, error) { // Fungsi untuk meng-inputkan data Post Method dari tabel product
 	var res Response // variable untuk menampung response nya
 
 	con := db.CreateCon() // inisialisasi koneksi database
 
 	sqlStatement := "INSERT INTO brands (brand_name) VALUES (?)" // query insert data
- 
+
 	stmt, err := con.Prepare(sqlStatement) // Variable persiapan awal sebelum eksekusi insert data
 
 	if err != nil {
-		return res,err 
+		return res, err
 	}
-	
+
 	//cek jika brand_name kosong.
 	if Brand_Name == "" {
-        return res, errors.New("Brand Name tidak boleh kosong.")
-    }
+		return res, errors.New("Brand Name tidak boleh kosong.")
+	}
 
 	result, err := stmt.Exec(Brand_Name) // Eksekusi insert data query
 
-
-	if err != nil{
-		return res, err 
+	if err != nil {
+		return res, err
 	}
 
 	lastInsertedId, err := result.LastInsertId()
 
-	
-	if err !=  nil {
+	if err != nil {
 		return res, err
 	}
 
 	res.Status = http.StatusOK
 	res.Message = "Success"
 	res.Data = map[string]int64{
-		"Id yang terakhir dimasukan adalah" : lastInsertedId,
+		"Id yang terakhir dimasukan adalah": lastInsertedId,
 	}
 
 	return res, nil
 
 }
 
-func DeleteBrand(Brand_Id int) (Response, error){ 
+func DeleteBrand(Brand_Id int) (Response, error) {
 	var res Response // variable untuk menampung response nya
 	//var obj Brands
 
 	con := db.CreateCon() // inisialisasi koneksi database
 
 	//cek jika brand_id tdk diisi.
-	if Brand_Id = nil {
+	if Brand_Id == 0 {
 		return res, errors.New("Brand ID tidak boleh kosong.")
 	}
 
 	sqlStatement2 := "DELETE FROM brands WHERE NOT EXISTS ( SELECT 1 FROM products WHERE brand_id = ?)" // Query hapus data jika brand_id tidak dipakai di product
 
-	stmt, err := con.Prepare(sqlStatement2) 
+	stmt, err := con.Prepare(sqlStatement2)
 	if err != nil {
-		return res,err 
+		return res, err
 	}
 
-	result, err := stmt.Exec(Brand_Id) 
+	result, err := stmt.Exec(Brand_Id)
 	if err != nil {
-		return res,err 
+		return res, err
 	}
 
 	rowsAffected, err := result.RowsAffected() // Meminta return dari berapa banyak rows yang terdampak kena penghapusan data
@@ -83,14 +81,14 @@ func DeleteBrand(Brand_Id int) (Response, error){
 	}
 
 	if err != nil {
-		return res,err // cek apakah saat penghapusan dari rows yang kena dampak ada error
+		return res, err // cek apakah saat penghapusan dari rows yang kena dampak ada error
 	}
 
 	// return sukses jika berhasil
-	res.Status = http.StatusOK 
-	res.Message = "Data berhasil dihapus" 
+	res.Status = http.StatusOK
+	res.Message = "Data berhasil dihapus"
 	res.Data = map[string]int64{
-		"Data terhapus sebanyak" : rowsAffected, // tampilkan berapa banyak row yang kena dampak 
+		"Data terhapus sebanyak": rowsAffected, // tampilkan berapa banyak row yang kena dampak
 	}
 
 	return res, nil
